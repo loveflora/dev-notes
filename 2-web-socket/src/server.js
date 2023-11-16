0fd5b64af9ebeae49afc4ca2e2d146c03f2b1360
saywhat <95282021+loveflora@users.noreply.github.com>
2023-11-16 23:05:57 +0900

    refactor: 개발환경 세팅

--- 2-web-socket/src/server.js
+++ 2-web-socket/src/server.js
@@ -1,5 +1,27 @@
 import express from "express";
+import livereloadMiddleware from "connect-livereload";
+import livereload from "livereload";
 
 const app = express();
 
-app.listen(3000);
+const liveServer = livereload.createServer({
+  exts: ["js", "pug", "css"],
+  delay: 1000,
+});
+
+liveServer.watch(__dirname);
+
+app.use(livereloadMiddleware());
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () => {
+  console.log(`Listen on http://localhost:3000`);
+};
+
+app.listen(3000, handleListen);
