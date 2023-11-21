c502508c3664f266d93e8d27d5f9be0853895596
saywhat <95282021+loveflora@users.noreply.github.com>
2023-11-21 17:16:47 +0900

    feat: web-socket

--- 2-web-socket/src/public/js/app.js
+++ 2-web-socket/src/public/js/app.js
@@ -1,19 +1,51 @@
+const msgList = document.querySelector('ul');
+const nickForm = document.querySelector('#nick');
+const msgForm = document.querySelector('#msg');
+
 // socket: 서버로의 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
-  console.log("Connected to Server");
+const makeMsg = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg); // {"type":"nickname","payload":"닉네임"}
+  //\ object 타입을 string 으로 바꾸는 이유
+  // javascript 서버가 아닐 수도 있기 때문 (javascript object로 보내면 해석 못함)
+};
+
+socket.addEventListener('open', () => {
+  console.log('Connected to Server');
 });
 
-socket.addEventListener("message", (message) => {
-  console.log("Just got this :", message.data, " from server");
+socket.addEventListener('message', (message) => {
+  console.log('Just got this :', message.data, ' from server');
+
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  msgList.append(li);
 });
 
 // server가 오프라인이면, browser한테 알려줌
-socket.addEventListener("close", () => {
-  console.log("Disconnected from Server");
+socket.addEventListener('close', () => {
+  console.log('Disconnected from Server');
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser");
-}, 2000);
+// setTimeout(() => {
+//   socket.send('hello from the browser');
+// }, 2000);
+
+const handleMsgSubmit = (e) => {
+  e.preventDefault();
+  const input = msgForm.querySelector('input');
+  socket.send(makeMsg('msg', input.value)); // front-end의 form에서 back-end로 전송
+  input.value = '';
+};
+
+const handleNickSubmit = (e) => {
+  e.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMsg('nickname', input.value));
+  input.value = '';
+};
+
+msgForm.addEventListener('submit', handleMsgSubmit);
+nickForm.addEventListener('submit', handleNickSubmit);
--- 2-web-socket/src/server.js
+++ 2-web-socket/src/server.js
@@ -1,14 +1,14 @@
-import express from "express";
-import http from "http";
-import WebSocket from "ws";
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
-import livereloadMiddleware from "connect-livereload";
-import livereload from "livereload";
+import livereloadMiddleware from 'connect-livereload';
+import livereload from 'livereload';
 
 const app = express();
 
 const liveServer = livereload.createServer({
-  exts: ["js", "pug", "css"],
+  exts: ['js', 'pug', 'css'],
   delay: 1000,
 });
 
@@ -16,12 +16,12 @@ liveServer.watch(__dirname);
 
 app.use(livereloadMiddleware());
 
-app.set("view engine", "pug");
-app.set("views", __dirname + "/views");
-app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('home'));
 
-app.get("/*", (req, res) => res.redirect("/"));
+app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => {
   console.log(`Listen on http://localhost:3000`);
@@ -31,6 +31,9 @@ const handleListen = () => {
 // http protocol과 ws 연결을 지원
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
+
+//-- db
+const sockets = [];
 
 // socket: 연결된 브라우저
 // function handleConnection(socket) {
@@ -42,20 +45,49 @@ const wss = new WebSocket.Server({ server });
 // wss : 전체 서버
 // socket : 특정 socket
 // 서버에 있는 메서드가 아닌, socket에 있는 메서드는 socket으로 직접적인 연결을 제공해줌
-wss.on("connection", (socket) => {
-  console.log("Connected to Browser");
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+
+  // nickname 미정일 경우
+  socket['nickname'] = 'Anonymous';
+
+  console.log('Connected to Browser');
 
   // browser 연결이 끊기면, server에 event 발생시킴
-  socket.on("close", () => {
-    console.log("Disconnected from Browser");
+  socket.on('close', () => {
+    console.log('Disconnected from Browser');
   });
 
-  socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
+  socket.on('message', (message) => {
+    // socket.send(message.toString('utf8'));  // 이것만 작성하면 다른 브라우저에서 확인 불가
+
+    // message
+    // {"type":"nickname","payload":"닉네임"}
+    const parsed = JSON.parse(message); // 다시 object로 변환
+
+    //-- switch
+    switch (parsed.type) {
+      case 'msg':
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname}: ${parsed.payload}`)
+        );
+        break;
+      case 'nickname':
+        socket['nickname'] = parsed.payload; // "nickname": 닉네임 (key-value 추가)
+        break;
+    }
+
+    //-- if ~ else
+    // if (parsed.type === 'msg') {
+    //   // 다른 브라우저에서도 확인 가능
+    //   sockets.forEach((aSocket) => aSocket.send(parsed.payload));
+    // } else if (parsed.type === 'nickname') {
+    //   console.log(parsed.payload);
+    // }
   });
 
   // connection이 생기면, socket으로 즉시 메세지를 보냄 (BE -> FE)
-  socket.send("hello!!!");
+  socket.send('hello!!!');
 });
 
 server.listen(3000, handleListen);
