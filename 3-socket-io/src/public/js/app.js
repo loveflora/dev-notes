7985782433d18288540beeed114c70cfd5c3f059
saywhat <95282021+loveflora@users.noreply.github.com>
2023-11-22 23:37:18 +0900

    feat: room message

--- 3-socket-io/src/public/js/app.js
+++ 3-socket-io/src/public/js/app.js
@@ -3,3 +3,44 @@ const socket = io();
 
 // Socket.io 는 연결된 모든 socket을 자동으로 추적함
 // sockets.push(socket); 을 적을 필요 없음
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName;
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
+}
+
+function handleRoomSubmit(e) {
+  e.preventDefault();
+  const input = form.querySelector("input");
+  // send가 아니라 emit
+
+  socket.emit(
+    "enter_room", // 1) event 이름
+    input.value, // 2) ~ 아무거나
+    showRoom, // 3) 마지막 : 함수
+    // 함수도 전송 가능
+  );
+  roomName = input.value;
+  // FE 에서 object 전송
+  input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+  const ul = room.querySelector("ul");
+  const li = document.createElement("li");
+  li.innerText = "Someone Joined !";
+  ul.appendChild(li);
+});
--- 3-socket-io/src/server.js
+++ 3-socket-io/src/server.js
@@ -32,7 +32,27 @@ const wsServer = SocketIO(httpServer);
 
 // FE와 연결
 wsServer.on("connection", (socket) => {
-  console.log(socket);
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+  // 여러 개를, 어떤 형식으로든 보낼 수 있음
+  // (하나를 string 형식만 보낼 수 있는게 아니라)
+  socket.on("enter_room", (roomName, done) => {
+    // console.log(roomName); // room (입력한 값)
+    // console.log(socket.id); // B7XNKRzCwh-X6oiLAAAB
+    socket.join(roomName);
+    // console.log(socket.rooms); // Set(2) { 'B7XNKRzCwh-X6oiLAAAB', 'room' }
+    done();
+
+    // "welcome" 이벤트를 roomName에 있는 모든 사람에게 emit
+    socket.to(roomName), emit("welcome");
+
+    // setTimeout(() => {
+    // BE가 실행을 시켜서(함수 호출), FE에서 해당 함수가 실행됨
+    // - BE애서 실행되는 것은 아님. 보안에 문제가 생길 수 있기 때문.
+    //   done("hello from the backend");
+    // }, 1000);
+  });
 });
 
 httpServer.listen(3000, handleListen);
