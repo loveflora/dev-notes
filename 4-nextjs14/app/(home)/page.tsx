5f5ebbe7cc920977dfe3bdde9e1ff256982d2bc1
saywhat <95282021+loveflora@users.noreply.github.com>
2024-02-19 17:05:13 +0900

    feat: metadata added

--- 4-nextjs14/app/(home)/page.tsx
+++ 4-nextjs14/app/(home)/page.tsx
@@ -1,12 +1,16 @@
-import Navigation from "../components/navigation";
+export const metadata = {
+  title: "Home ",
+};
 
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>Hello !</h1>
     </div>
   );
 }
 
 // root segment ( Home / )
+
+// route group
+// (괄호) 표시는 url에 영향 주지 않음.
