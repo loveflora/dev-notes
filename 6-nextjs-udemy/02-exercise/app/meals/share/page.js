b2a2fb030f48d88a133c3781cddd05ce11aa1ebb
saywhat <95282021+loveflora@users.noreply.github.com>
2024-10-31 06:53:52 +0900

    feat: file upload

--- 6-nextjs-udemy/02-exercise/app/meals/share/page.js
+++ 6-nextjs-udemy/02-exercise/app/meals/share/page.js
@@ -1,10 +1,20 @@
-// 'use client'
+"use client";
 
+import { useActionState } from "react-dom";
 import ImagePicker from "../../../components/meals/image-picker";
-import { shareMeal } from "../../../lib/action";
+import MealsFormSubmit from "../../../components/meals/meals-form-submit";
+import { shareMeal } from "../../../lib/actions";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  const [state, formAction] = useActionState(shareMeal, { message: null });
+  //< [state, formAction]
+  // state : 상태 - (미)응답값
+  // formAction : form 태그의 action 속성값
+  //< useActionState(1, 2)
+  // 1 : form 이 제출할 때 동작하는 실제 Server Action
+  // 2 : 컴포넌트의 초기 state(상태), shareMeal action이 동작하기 전이나 response가 돌아오기 전에 useFormState가 반환할 초기값
+
   //>> action.js 파일로 Server Action 따로 분리
   // 그렇게 안하면 상단에 'use client' 입력 시, 에러남.
   // 같은 파일에 client, server 두 종류 모두 있으면 -> 서버측 코드가 클라이언트에 위치해서 보안문제가 생길 수 있음
@@ -40,7 +50,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           {/* action={shareMeal} */}
           {/* -> Server Action 설정 */}
           <div className={classes.row}>
@@ -72,7 +82,8 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            {/* <button type="submit">Share Meal</button> */}
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
--- 6-nextjs-udemy/02-exercise/lib/meals.js
+++ 6-nextjs-udemy/02-exercise/lib/meals.js
@@ -33,7 +33,7 @@ export function getMeal(slug) {
   // + slug : 불안정함. SQL Injection 에 노출될 위험이 있음
 }
 
-export function saveMeal(meal) {
+export async function saveMeal(meal) {
   //_ 1) meal 의 slug 속성에 추가
   // 설정 객체 --> '{ lower: true }' --> 모든 문자를 소문자로
   meal.slug = slugify(meal.title, { lower: true });
@@ -52,7 +52,7 @@ export function saveMeal(meal) {
   // DB에 파일 저장 X
   // -> 업로드된 파일을 public 폴더에 저장하려 함
   // -> public > images 폴더에 저장된 이미지들은 어디서든 접근용이
-  const extension = meal.image.split(".").pop();
+  const extension = meal.image.name.split(".").pop();
   // meal.image --> action.js 파일 참고 (image가 key임)
   // .split(".").pop() --> 마지막 요소(확장자)만 받음
   const fileName = `${meal.slug}.${extension}`;
@@ -60,7 +60,42 @@ export function saveMeal(meal) {
   //_ 4) public 폴더에 파일을 write (import fs)
   // 어떤 파일에 데이터 쓸 수 있도록 해주는 stream 생성
   // `경로/파일명`
-  fs.createWriteStream(`public/images/${fileName}`);
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
 
-  stream.write();
+  //< arrayBuffer
+  // - buffer로 변환 -> write() 함수가 동작하는데 필요
+  // - Promise를 반환하기 때문에, 'await' 키워드 필요
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  //< write() 함수
+  // - (method) internal.Writable.write(chunk: any, callback?: (error: Error) => void): boolean (+1 overload)
+  // - 첫번째 인수: 저장할 파일
+  //   두번째 인수: 쓰기를 마치면, 실행될 함수
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Saving image failed!");
+    }
+  });
+
+  // DB에 이미지 객체가 아닌, 파일 경로만 저장하기 위해서 덮어씀.
+  // public 세그먼트는 제거 (> 모든 이미지에 관한 요청은 자동으로 public 폴더로 보내지기 때문)
+  meal.image = `/images/${fileName}`;
+
+  // 저장
+  // 각 필드에 맞는 데이터가 추출됨
+  db.prepare(
+    `
+  INSERT INTO meals
+    (title, summary, instructions, creator, creator_email, image, slug)
+  VALUES (
+    @title,
+    @summary,
+    @instructions,
+    @creator,
+    @creator_email,
+    @image,
+    @slug
+    )
+  `,
+  ).run(meal);
 }
