44e7d9c9c9d61b6469cd815f5e021bc0eeafa213
saywhat <95282021+loveflora@users.noreply.github.com>
2025-02-26 17:05:48 +1100

    Feat: update layout

--- 8-react-native/my-app/app/(tabs)/index.tsx
+++ 8-react-native/my-app/app/(tabs)/index.tsx
@@ -8,16 +8,28 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { TextInput } from "react-native-gesture-handler";
 
+import { useColorScheme } from "@/hooks/useColorScheme";
+
 export default function HomeScreen() {
+  const colorScheme = useColorScheme();
+  const dynamicStyles = StyleSheet.create({
+    description: {
+      fontSize: 16,
+      color: colorScheme === "dark" ? "#fff" : "#000",
+    },
+  });
+
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course goal!" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
         <Button title="Add Goal" />
       </View>
-      <View>
-        <Text>Life of Goal...</Text>
+
+      <View style={styles.goalsContainer}>
+        <Text style={dynamicStyles.description}>Life of Goal..?</Text>
       </View>
+      {/* <Image source={}/> */}
       {/* <Button title="Tap me!" /> */}
     </View>
 
@@ -38,6 +50,27 @@ export default function HomeScreen() {
 // StyleSheet: 자동완성
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    paddingTop: 50,
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    marginBottom: 24,
+  },
+  textInput: {
+    width: "70%",
+    borderWidth: 1,
+    padding: 8,
+    marginRight: 8,
+    borderColor: "#ccc",
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
