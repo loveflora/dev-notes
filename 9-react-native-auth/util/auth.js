ef5d8fae68f1959b1d592b78176a58ead2f023a8
saywhat <95282021+loveflora@users.noreply.github.com>
2025-03-04 22:06:19 +1100

    Feat: Implement login functionality

--- 9-react-native-auth/util/auth.js
+++ 9-react-native-auth/util/auth.js
@@ -2,16 +2,25 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyD3OZW3DOAZv2FzdWTsLYk5bvwytIbkW-U";
 
+async function authenticate(mode, email, password) {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+
+  const response = await axios.post(url, {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  });
+
+  console.log(response.data);
+}
+
 //_ 비동기 함수 (async, await) - promise 반환
 // promise가 성공(resolve)할 때까지, 대기(await)
 // -> 로딩 오버레이 표시
 export async function createUser(email, password) {
-  const response = await axios.post(
-    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-    {
-      email: email,
-      password: password,
-      returnSecureToken: true,
-    },
-  );
+  await authenticate("signUp", email, password);
+}
+
+export async function login(email, password) {
+  await authenticate("signInWithPassword", email, password);
 }
